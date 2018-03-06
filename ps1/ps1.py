bf3b898f3e90c3832eb5326b8c7ab461d3d5caf1
FanciesFancyRats <magriffi1986@gmail.com>
2018-03-05 18:12:00 -0600

    first commit

---
+++ ps1/ps1.py
@@ -0,0 +1,7 @@
+annual_salary = input("Please input your annual salary: ")
+portion_saved = input("Please input your portion saved: ")
+total_cost = input("Please input the total cost: ")
+portion_down_payment = .25 * total_cost
+while (portion_down_payment > current_savings):
+
+
